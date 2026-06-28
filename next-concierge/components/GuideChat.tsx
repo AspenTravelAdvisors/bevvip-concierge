2d37a03452018f17cc8fe7b15dbcf297e5e5ff8a
AspenTravelAdvisors <aspentraveladvisors@gmail.com>
2026-06-28 13:11:09 -0600

    Update GuideChat.tsx

--- next-concierge/components/GuideChat.tsx
+++ next-concierge/components/GuideChat.tsx
@@ -17,7 +17,7 @@ const CHIPS = [
   "Galápagos Expedition Cruise journeys in January",
   "Aman vs. Orient Express Luxury Yachts",
   "Around the world by private jet trips in 2026",
-  "World Cruises that call on Sydney in 2027",
+  "World Cruises that call on Sydney, Australia in 2027",
 ];
 
 interface Turn extends ChatMessage {
